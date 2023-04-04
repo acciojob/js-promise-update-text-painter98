b696685c46fbaef7903801e3b84be1a1acb369b9
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-04 13:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function output(){
+	return promise;
+}
+let promise = new Promise((resolve,reject) => {
+	setTimeout(function(){
+		resolve("Hello, world!");
+	},1000);
+});
+console.log(output());
