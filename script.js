b83bc91e0ba1c74bdbfc8c51544196768d3b00f1
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-04 14:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 //your JS code here. If required.
-let promise = new Promise((resolve,reject) => {
+/*let promise = new Promise((resolve,reject) => {
 	setTimeout(function(){
 		resolve("Hello, world!");
 	},1000);
 }); 
 promise.then((data) => {
 	 document.getElementById('output').innerText = data;
+});*/
+function updateOutput() {
+return new Promise((resolve, reject) => {
+setTimeout(() => {
+resolve("Hello, world!");
+}, 1000);
+});
+}
+
+const outputElement = document.getElementById("output");
+updateOutput().then((message) => {
+outputElement.innerText = message;
 });
